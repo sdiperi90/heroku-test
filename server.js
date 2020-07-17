c1ae48e0640ac5290836269bc149ad5442bfeeca
Saida DiPeri <sdiperi@Saidas-MBP.cable.rcn.com>
2020-07-17 15:41:57 -0400

    added post route

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const { google } = require("googleapis");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
+  console.log("TEST", google.gmail);
   res.sendFile("/index.html", { root: __dirname });
+});
+
+app.post("/", async (req, res) => {
+  const resp = await google.gmail.users.watch({
+    userId: "sirgasheva17@gmail.com",
+    requestBody: {
+      // Replace with `projects/${PROJECT_ID}/topics/${TOPIC_NAME}`
+      topicName: `projects/little-leo-tadpoles/topics/tadpolesGmailTrigger`
+    }
+  });
+  console.log(resp.data);
+  console.log("REQ", req);
+
+  return res.send("success");
 });
 
 app.listen(PORT, () =>
