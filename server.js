b6be83c5a29684f3f7a3e0e5549c1a5a84583dc2
Saida DiPeri <sdiperi@Saidas-MBP.cable.rcn.com>
2020-07-17 13:30:44 -0400

    sending index html file

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Hello Shamu!"));
+app.get("/", (req, res) => {
+  res.sendFile("/index.html", { root: __dirname });
+});
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
