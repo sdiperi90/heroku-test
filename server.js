cec42a9a6fa820cde1533f3331118ce58879d136
Saida DiPeri <sdiperi@Saidas-MBP.cable.rcn.com>
2020-07-17 12:40:52 -0400

    fixed port

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.send("Hello Shamu!"));
 
-app.listen(port, () =>
+app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
