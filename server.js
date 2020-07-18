7cd24eac9e261aacae21e1216364a0371d10e2b2
Saida DiPeri <sdiperi@Saidas-MBP.cable.rcn.com>
2020-07-17 22:04:45 -0400

    test2

--- server.js
+++ server.js
@@ -3,13 +3,11 @@ const { google } = require("googleapis");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  console.log("TEST", google.gmail);
-  res.sendFile("/index.html", { root: __dirname });
-});
-
-app.post("/", async (req, res) => {
-  const gmail = await google.gmail({ version: "v1", auth });
+app.get("/", async (req, res) => {
+  const gmail = await google.gmail({
+    version: "v1",
+    auth: process.env.API_KEY
+  });
   let resp = await gmail.users.watch({
     userId: "sdiperi17@gmail.com",
     requestBody: {
@@ -25,7 +23,35 @@ app.post("/", async (req, res) => {
       "CHAT",
       "SPAM"
     ],
-    labelFilterAction: "exclude"
+    labelFilterAction: "include"
+  });
+
+  console.log("resp", resp);
+  console.log("TEST", gmail);
+  res.sendFile("/index.html", { root: __dirname });
+});
+
+app.post("/", async (req, res) => {
+  const gmail = await google.gmail({
+    version: "v1",
+    auth: process.env.API_KEY
+  });
+  let resp = await gmail.users.watch({
+    userId: "sdiperi17@gmail.com",
+    requestBody: {
+      topicName: "projects/little-leo-tadpoles/topics/tadpolesGmailTrigger"
+    },
+    labelIds: [
+      "CATEGORY_UPDATES",
+      "DRAFT",
+      "CATEGORY_PROMOTIONS",
+      "CATEGORY_SOCIAL",
+      "CATEGORY_FORUMS",
+      "TRASH",
+      "CHAT",
+      "SPAM"
+    ],
+    labelFilterAction: "include"
   });
 
   console.log(resp.data);
